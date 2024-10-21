277db19eb70dd174b60ba7ef4631b46aeed3ecd1
sheharyarghouri <mshahryarkghouri@gmail.com>
2024-10-21 23:30:59 +0500

    In Class task pushing objects in array for practice

---
+++ app.js
@@ -0,0 +1,48 @@
+
+var obj = [];
+
+function signup() {
+
+    var arr = {
+        userName: document.getElementById("userName").value,
+        userLastName: document.getElementById("userLastName").value,
+        userEmail: document.getElementById("userEmail").value,
+        userPassword: document.getElementById("userPassword").value,
+    }
+    obj.push(arr);
+
+    console.log(obj);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var practice = {
+//     name : "waqas",
+//     age : "18",
+//     pocketMoney : 500+1000+100,
+// };
+
+// // console.log(practice.name)
+// // console.log(practice.age)
+// // console.log(practice.pocketMoney)
+
+
+// var userName = prompt ("Enter your name");
+// var userAge = prompt ("Enter your age");
+// var userPocketMoney = prompt ("Enter your pocket money");
+
+// practice.name
+
+// console.log(practice)
